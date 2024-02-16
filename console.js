570319f04b3b156ce92cf60a64867331720c9324
Wilsonmobisa <willexjumah12338@gmail.com>
2024-02-17 00:05:12 +0300

    link

---
+++ console.js
@@ -0,0 +1,4 @@
+var x = 5,
+  y = 5;
+var z = "x * y";
+console.log(eval(z));
---
+++ script.js
@@ -0,0 +1,11 @@
+// https://www.codingnepalweb.com/calculator-html-css-javascript/
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
+
+//Define function to calculate based on button clicked.
+const calculate = (btnValue) => {
+  display.focus();
+  
+};
